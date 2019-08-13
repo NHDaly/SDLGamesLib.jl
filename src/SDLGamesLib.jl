e0b2b96f41f1f4398c58292cac11d9eab1eb8e7f
Nathan Daly <nhdaly@gmail.com>
2019-08-13 14:36:35 -0400

    Create julia 1.0 package structure

---
+++ src/SDLGamesLib.jl
@@ -0,0 +1,10 @@
+module SDLGamesLib
+
+using SimpleDirectMediaLayer
+SDL2 = SimpleDirectMediaLayer
+
+include("objects.jl")
+include("display.jl")
+#include("pixel.jl")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SDLGamesLib
+using Test
+
+@testset "SDLGamesLib.jl" begin
+    # Write your own tests here.
+end
